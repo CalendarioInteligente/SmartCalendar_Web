a6ae616cd6d93827465ade1f2bc172fe1a763ae7
Gabriel <gabriel.ap@outlook.com>
2023-11-11 12:43:38 -0300

    Alinhando texto verticalmente e outras coisas

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,16 +3,18 @@ import './About.css'
 const About = () => {
   return (
     <div id="about">
-      <h1>Smart Calendar</h1>
-      <p>
-      &emsp; Desperte o potencial do seu dia-a-dia<br/>
-        com o Smart Calendar - sua chave para<br/>
-        uma rotina fluida e organizada. Com<br/>
-        recursos intuitivos e personalizados,<br/>
-        otimize seu tempo e alcance seus objetivos<br/>
-        com facilidade. Viva com propósito<br/>
-        agende com inteligência!<br/>
-      </p>
+      <span>
+        <h1>Smart Calendar</h1>
+        <p>
+        &emsp; Desperte o potencial do seu dia-a-dia<br/>
+          com o Smart Calendar - sua chave para<br/>
+          uma rotina fluida e organizada. Com<br/>
+          recursos intuitivos e personalizados,<br/>
+          otimize seu tempo e alcance seus objetivos<br/>
+          com facilidade. Viva com propósito<br/>
+          agende com inteligência!<br/>
+        </p>
+      </span>
     </div>
   )
 }
